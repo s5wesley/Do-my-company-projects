c4213ad848547d8fdaadab5777bec4bd90dbbddf
Harold Mbarga <mbah00@hotmail.com>
2025-03-31 23:01:39 -0400

    created cloud run app

---
+++ cloud_run_service/variables.tf
@@ -0,0 +1,21 @@
+variable "project_id" {
+  description = "GCP project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "GCP region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "service_name" {
+  description = "Cloud Run service name"
+  type        = string
+  default     = "rock-band-service"
+}
+
+variable "container_image" {
+  description = "Docker container image URL"
+  type        = string
+}
